bbd51b4ab3b4d76f457cde58840948f72e6b1dba
BIBS777 <156431444+BIBS777@users.noreply.github.com>
2024-03-29 17:36:22 +0545

    added files

---
+++ script.js
@@ -0,0 +1,40 @@
+function toogleForms(){
+var box1 = document.querySelector('.box1');
+var box2 = document.querySelector('.box2');
+box1.style.display ='none';
+box2.style.display ='block';
+}
+function tooglesign(){
+    var box1 = document.querySelector('.box1');
+    var box2 = document.querySelector('.box2');
+    box2.style.display ='none';
+    box1.style.display ='block';
+    }
+    function toogleForget(){
+        var box1 = document.querySelector('.box1');
+        var box2 = document.querySelector('.box2');
+        var Forget = document.querySelector('.Forget');
+        box2.style.display ='none';
+        box1.style.display ='none';
+        Forget.style.display ='block';
+        }
+        function tooglebackLogin(){
+            var box1 = document.querySelector('.box1');
+            var box2 = document.querySelector('.box2');
+        var Forget = document.querySelector('.Forget');
+            Forget.style.display ='none';
+            box2.style.display ='none';
+            box1.style.display ='block';
+            }
+            function tooglebackPopup(){
+                var box1 = document.querySelector('.box1');
+                var box2 = document.querySelector('.box2');
+            var Forget = document.querySelector('.Forget');
+            var Popup = document.querySelector('.Popup');
+
+                Forget.style.display ='none';
+                box2.style.display ='none';
+                box1.style.display ='none';
+                Popup.style.display ='block';
+
+                }
